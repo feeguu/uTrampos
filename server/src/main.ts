710defe95784df31c93754ab28d449a5091f993e
feeguu <gusiqueira2010@gmail.com>
2023-09-27 14:35:26 -0300

    Swagger integration and test configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/modules/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('uTrampos API')
+    .setDescription('Lado servidor do uTrampos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
