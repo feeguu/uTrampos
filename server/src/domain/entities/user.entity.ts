57d1db94b6951b2c86d561a730e4dfad8e3653b4
feeguu <gusiqueira2010@gmail.com>
2023-09-28 15:34:14 -0300

    ref: user entity

--- server/src/domain/entities/user.entity.ts
+++ server/src/domain/entities/user.entity.ts
@@ -2,14 +2,11 @@ import { randomUUID } from 'crypto';
 
 export class User {
   id: string;
-  name: string;
-  email: string;
-  password: string;
-
-  constructor(name: string, email: string, password: string) {
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+  ) {
     this.id = randomUUID();
-    this.name = name;
-    this.email = email;
-    this.password = password;
   }
 }
