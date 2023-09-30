c49ff6ae3fc9c534e180305197f4282a19a4778e
FATEC ZONA LESTE <kaykysousa321@gmail.com>
2023-09-30 12:07:02 -0300

    feat: fetch register api route

--- frontend/src/pages/register.vue
+++ frontend/src/pages/register.vue
@@ -1,11 +1,40 @@
 <script setup lang="ts">
 import { RiFacebookFill, RiGoogleFill } from "vue-remix-icons"
+
+const register = reactive({
+	email: "",
+	password: "",
+	name: "NameTest",
+	phone: "11923451234",
+	zipCode: "08081-530",
+	type: "candidate"
+})
+
+async function registerUser() {
+	try {
+		const res: any = await $fetch("http://localhost:3001/auth/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(register)
+		})
+
+		const token = useCookie("utrampos.token")
+		token.value = res.token
+
+		navigateTo("/")
+	} catch (error) {
+		console.error(error)
+	}
+}
+
 </script>
 
 <template>
 	<div class="bg-neutral-100 px-6 w-full min-h-screen flex flex-col justify-center items-center">
 		<Logo class="mb-12" />
-		<form class="flex flex-col gap-y-4 w-full max-w-md">
+		<form class="flex flex-col gap-y-4 w-full max-w-md" @submit.prevent="registerUser">
 			<div class="flex flex-col gap-y-2">
 				<p class="text-slate-900 font-semibold">Sou um:</p>
 				<label
@@ -27,12 +56,12 @@ import { RiFacebookFill, RiGoogleFill } from "vue-remix-icons"
 			</div>
 			<Separator>ou continue com</Separator>
 
-			<Input label-text="E-mail" />
-			<Input label-text="Senha" />
-			<Button>Cadastrar</Button>
+			<Input label-text="E-mail" v-model="register.email" />
+			<Input label-text="Senha" v-model="register.password" />
+			<Button type="submit">Cadastrar</Button>
 
 			<NuxtLink to="/login" class="text-slate-900 text-sm text-center font-semibold">
-				Já possui uma conta? <span class="text-sky-600">Entre</span>
+				Já possui uma conta? <span class="text-sky-500">Entre</span>
 			</NuxtLink>
 		</form>
 	</div>
