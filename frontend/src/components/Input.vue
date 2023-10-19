64f2d9d772dceae954baf0640030bd7836426b62
KaykySousa <kaykysousa321@gmail.com>
2023-10-18 21:38:45 -0300

    refactor: move emits types to an interface and change border input color on focus

--- frontend/src/components/Input.vue
+++ frontend/src/components/Input.vue
@@ -4,15 +4,22 @@ interface InputProps {
 	modelValue?: string
 }
 
-defineProps<InputProps>()
-defineEmits<{
+interface InputEmits {
 	(event: "update:modelValue", value: string): void
-}>()
+}
+
+defineProps<InputProps>()
+defineEmits<InputEmits>()
 </script>
 
 <template>
 	<label class="flex flex-col text-slate-900">
-		<span class="font-semibold mb-1" v-if="labelText">{{ labelText }}</span>
-		<input type="text" class="border-gray-300 rounded h-12" :value="modelValue" @input="$emit('update:modelValue', ($event.target as any).value)" />
+		<span class="font-semibold mb-2 leading-none" v-if="labelText">{{ labelText }}</span>
+		<input
+			type="text"
+			class="border-gray-300 rounded h-12 focus:!border-sky-500 focus:!ring-sky-500"
+			:value="modelValue"
+			@input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+		/>
 	</label>
 </template>
