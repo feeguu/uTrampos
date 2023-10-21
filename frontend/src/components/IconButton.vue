9909b68456b59512e80de3c53ed6c9bfe9ffe3e0
KaykySousa <kaykysousa321@gmail.com>
2023-10-20 21:46:25 -0300

    feat: add click emit in icon button

--- frontend/src/components/IconButton.vue
+++ frontend/src/components/IconButton.vue
@@ -1,5 +1,8 @@
 <template>
-	<button class="p-1 -m-1 rounded-full transition-colors hover:bg-neutral-200">
+	<button
+		@click="$emit('click', $event)"
+		class="p-1 -m-1 rounded-full transition-colors hover:bg-neutral-200"
+	>
 		<slot />
 	</button>
 </template>
