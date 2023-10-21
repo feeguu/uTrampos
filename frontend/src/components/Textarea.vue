9d85ff01cdc188ddd90471bdbd6885f9d5fff023
KaykySousa <kaykysousa321@gmail.com>
2023-10-20 21:49:35 -0300

    feat: create textarea component

--- frontend/src/components/Textarea.vue
+++ frontend/src/components/Textarea.vue
@@ -16,7 +16,6 @@ defineEmits<TextAreaEmits>()
 	<label class="flex flex-col text-slate-900">
 		<span class="font-semibold mb-2 leading-none" v-if="labelText">{{ labelText }}</span>
 		<textarea
-			type="text"
 			class="border-gray-300 rounded h-24 min-h-[3rem] focus:!border-sky-500 focus:!ring-sky-500"
 			:value="modelValue"
 			@input="$emit('update:modelValue', ($event.target as HTMLTextAreaElement).value)"
