ad43e1ff867313784c2e43a478c528e4043052e8
KaykySousa <kaykysousa321@gmail.com>
2023-10-23 22:36:28 -0300

    fix: set options as optional

--- frontend/src/composables/useAPI.ts
+++ frontend/src/composables/useAPI.ts
@@ -1,13 +1,13 @@
-export const useAPI = async <ResponseType>(request: string, options: any) => {
+export const useAPI = async <ResponseType>(request: string, options?: any) => {
 	const token = useToken().get()
 
 	const res = await useFetch<ResponseType>(`http://localhost:3001${request}`, {
+		...options,
 		headers: {
 			"Content-Type": "application/json",
 			Authorization: token ? `Bearer ${token}` : "",
-			...options.headers,
+			...options?.headers,
 		},
-		...options,
 	})
 
 	return {
