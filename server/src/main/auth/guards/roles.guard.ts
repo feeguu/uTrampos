ad8a059562acdac1d154ba4375364566528f175f
feeguu <gusiqueira2010@gmail.com>
2023-10-25 12:22:12 -0300

    fix: roles guard check if company/candidate has been created

--- server/src/main/auth/guards/roles.guard.ts
+++ server/src/main/auth/guards/roles.guard.ts
@@ -3,11 +3,17 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { UserDto } from '@/presentation/dtos/user.dto';
+import { TypeOrmCompanyRepository } from '@/infra/db/typeorm/repositories/typeorm-company.repository';
+import { TypeOrmCandidateRepository } from '@/infra/db/typeorm/repositories/typeorm-candidate.repository';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+  constructor(
+    private reflector: Reflector,
+    private readonly companyRepository: TypeOrmCompanyRepository,
+    private readonly candidateRepository: TypeOrmCandidateRepository,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<UserType[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
@@ -16,6 +22,14 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user }: { user: UserDto } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.type === role);
+    const userType = user.type;
+    if (userType === UserType.COMPANY) {
+      const company = await this.companyRepository.findByUserId(user.id);
+      return requiredRoles.some((role) => user.type === role && company);
+    }
+    if (userType === UserType.CANDIDATE) {
+      const candidate = await this.candidateRepository.findByUserId(user.id);
+      return requiredRoles.some((role) => user.type === role && candidate);
+    }
   }
 }
