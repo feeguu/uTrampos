b3988926969c451298baad13a0d56fa09e7b70fb
feeguu <gusiqueira2010@gmail.com>
2023-10-26 22:09:06 -0300

    fix: profile route when company/candidate not found

--- server/src/main/profile/get-profile-use-case.service.ts
+++ server/src/main/profile/get-profile-use-case.service.ts
@@ -1,31 +1,30 @@
 import { CandidateRepository } from '@/domain/abstracts/repositories/candidate.repository';
 import { CompanyRepository } from '@/domain/abstracts/repositories/company.repository';
 import { UserType } from '@/domain/enums/user-type.enum';
-import { CandidateDto } from '@/presentation/dtos/candidate.dto';
-import { CompanyDto } from '@/presentation/dtos/company.dto';
 import { ProfileDto } from '@/presentation/dtos/profile.dto';
 import { UserDto } from '@/presentation/dtos/user.dto';
-import { CandidateMapper } from '@/presentation/mappers/candidate.mapper';
-import { CompanyMapper } from '@/presentation/mappers/company.mapper';
-
 export class GetProfileUseCase {
   constructor(
     private readonly companyRepository: CompanyRepository,
     private readonly candidateRepository: CandidateRepository,
   ) {}
   async execute(user: UserDto): Promise<ProfileDto> {
+    console.log(user);
     const res: ProfileDto = {
       user: user,
     };
     if (user.type === UserType.CANDIDATE) {
-      const { user: _, ...candidate } =
-        await this.candidateRepository.findByUserId(user.id);
-      if (candidate) res.candidate = candidate;
+      const candidate = await this.candidateRepository.findByUserId(user.id);
+      if (candidate) {
+        const { user: _, ...candidateWithoutUser } = candidate;
+        res.candidate = candidateWithoutUser;
+      }
     } else {
-      const { user: _, ...company } = await this.companyRepository.findByUserId(
-        user.id,
-      );
-      if (company) res.company = company;
+      const company = await this.companyRepository.findByUserId(user.id);
+      if (company) {
+        const { user: _, ...companyWithoutUser } = company;
+        res.company = companyWithoutUser;
+      }
     }
     return res;
   }
