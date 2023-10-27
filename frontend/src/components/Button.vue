66cb39929b959d3c2354ce1cf9f0d61afafb053d
KaykySousa <kaykysousa321@gmail.com>
2023-10-26 21:58:49 -0300

    feat: add themes in button components

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+const themes = {
+	primary:
+		"w-full bg-slate-900 flex justify-center gap-x-1 items-center h-12 px-4 text-white rounded font-semibold",
+	secondary:
+		"w-full bg-sky-500 flex justify-center gap-x-1 items-center h-12 px-4 text-white rounded font-semibold",
+}
+
+interface ButtonProps {
+	theme?: keyof typeof themes
+}
+
+defineProps<ButtonProps>()
+</script>
+
 <template>
-	<button
-		type="button"
-		@click="$emit('click', $event)"
-		class="w-full bg-slate-900 flex justify-center gap-x-1 items-center h-12 px-4 text-white rounded font-semibold"
-	>
+	<button type="button" @click="$emit('click', $event)" :class="themes[theme ?? 'primary']">
 		<slot />
 	</button>
 </template>
--- frontend/src/components/IconButton.vue
+++ frontend/src/components/IconButton.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+const themes = {
+	primary: "p-1 -m-1 rounded-full transition-colors hover:bg-neutral-200",
+	defaultPrimary:
+		"h-12 w-12 bg-slate-900 flex justify-center items-center text-white rounded font-semibold",
+	defaultSecondary:
+		"h-12 w-12 bg-sky-500 flex justify-center items-center text-white rounded font-semibold",
+}
+
+interface IconButtonProps {
+	theme?: keyof typeof themes
+}
+
+defineProps<IconButtonProps>()
+</script>
+
 <template>
-	<button
-		@click="$emit('click', $event)"
-		class="p-1 -m-1 rounded-full transition-colors hover:bg-neutral-200"
-	>
+	<button @click="$emit('click', $event)" :class="themes[theme ?? 'primary']">
 		<slot />
 	</button>
 </template>
