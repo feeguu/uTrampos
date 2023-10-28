baac2c1649cdc36361c4f10bbb6acbd10ad2115a
KaykySousa <kaykysousa321@gmail.com>
2023-10-27 21:56:04 -0300

    feat: create provisory main company page

---
+++ frontend/src/pages/company/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { user, company } = useAuth()
+</script>
+<template>
+	<div class="flex-1 flex flex-col justify-center items-center">
+		<p class="text-slate-900 text-lg font-semibold mb-4">{{ user.name }}</p>
+		<LinkButton href="/jobs/create" class="!w-fit">Cadastrar Vaga</LinkButton>
+	</div>
+</template>
